50be397509b81ec0969a0cb9fc00a6d18301a6ff
An0nAN4N7 <pandeyanant733@gmail.com>
2025-07-18 00:57:06 +0530

    modified to webp

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,7 +107,9 @@ function initSvgIconAnimation() {
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C7.03 2 3 6.03 3 11c0 4.64 3.41 8.47 8 8.94V22h2v-2.06c4.59-.47 8-4.3 8-8.94 0-4.97-4.03-9-9-9zm0 16c-3.86 0-7-3.14-7-7 0-3.86 3.14-7 7-7s7 3.14 7 7c0 3.86-3.14 7-7 7z"/></svg>`,
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4S14.21 4 12 4 8 5.79 8 8s1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>`,
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2a9.93 9.93 0 0 0-7.07 2.93A10.01 10.01 0 0 0 2 12c0 2.21.71 4.25 1.93 5.93L2 22l4.07-1.93A9.93 9.93 0 0 0 12 22a10 10 0 1 0 0-20z"/></svg>`,
-    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 1L3 5v6c0 5.25 3.25 10 9 12 5.75-2 9-6.75 9-12V5l-9-4z"/></svg>`
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 1L3 5v6c0 5.25 3.25 10 9 12 5.75-2 9-6.75 9-12V5l-9-4z"/></svg>`,
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 2v2h6V2h2v2h3a1 1 0 0 1 1 1v3h-2V6H5v2H3V5a1 1 0 0 1 1-1h3V2h2zm12 10v2h-2v-2h2zM5 12v2H3v-2h2zm6 7v2H9v-2h6v2h-2v-2h-2z"/></svg>`,
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M1 21h22L12 2 1 21zm12-3h-2v2h2v-2zm0-6h-2v4h2v-4z"/></svg>`
   ];
 
   function changeIcon() {
@@ -116,7 +118,7 @@ function initSvgIconAnimation() {
   }
 
   changeIcon(); // Set an icon immediately on page load
-  setInterval(changeIcon, 2500); // Change icon every 2.5 seconds
+  setInterval(changeIcon, 2000); // Change icon every 2 seconds
 }
 
 /**
